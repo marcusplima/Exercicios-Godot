a8fed1598fee4367ad451fa30630f1666d1b5aa8
Marcus <marcusplima@gmail.com>
2019-09-13 23:28:27 -0300

    Método de cálculo de movimento corrigido. Falta inserir funções para todos os tipos de movimentos. O novo método foi calculado utilizando funções de geometria analítica Deve-se tornar as funções genéricas para todos os movimentos possíveis.

--- hora17-meu tomba bloco/Block.gd
+++ hora17-meu tomba bloco/Block.gd
@@ -1,142 +1,71 @@
 extends Spatial
 
+var Position1 = Transform( Vector3(1, 0, 0), Vector3(0, 1, 0), Vector3(0, 0, 1), Vector3(0, 1, 0) )
+var Position2 = Transform( Vector3(1, 0, 0), Vector3(0, 1, 0), Vector3(0, 0, 1), Vector3(0, 1, 0) )
+
+var angulo = 0.0
+var x_ini = 0.0
+var y_ini
+var x_end = 0.0
+var y_end
+var x_center = 0.0
+var y_center = 0.0
+var z_center = 0.0
+var position
 var rotating = false
 
-var current_position
-var direction
-var rotate_axis
-var offset
-var next_position
-var trans_x = 0
-var trans_y = 0
-var trans_z = 0
-var angulo = 0
-var corr = 0.5 / 90
-
 func _ready():
-	current_position = 'standing'
-	get_node("RigidBody").translate(Vector3(0,3,0))
+	#y_ini = y_circunferencia(x_ini)
+	#Position1.origin = Vector3(x_ini, y_ini, 0)
+	#rotation = Vector3(0, 0, rad2deg(0))
+	#transform = Position1
+	position = 'standing'
 
 func _input(event):
-	#para cada posição do bloco, o position3d deve ser reposicionado de acordo com o botao apertado
+	if event.is_action_pressed("ui_up"):
+		Position1.origin = Vector3(x_ini, y_ini, 0)
+		#Position1 = Position1.rotated(Vector3(0, 0, 1), deg2rad(0))#-90.0/100.0))#angulo_ini))
+		
+		x_end = 1.5
+		y_end = 0.5
+		Position2.origin = Vector3(x_end, y_end, 0)
+		#Position2 = Position2.rotated(Vector3(0, 0, 1), -0.5)#PI/2)
+		transform = Position1.interpolate_with(Position2, 1.0)
+		rotation_degrees = Vector3(0, 0, -90)
+		
 	if event.is_action_pressed("ui_down"):
 		print('pressed down')
-		match current_position:
-			'standing':
-				direction = -1
-				rotate_axis = 'z'
-				trans_y = 3
-				next_position = 'laid_front'
-				trans_x = 0
-				trans_z = 0
-			'laid_right':
-				pass
-			'laid_left':
-				pass
-			'laid_back':
-				direction = -1
-				rotate_axis = 'z'
-				trans_y = -3
-				next_position = 'standing'
-				trans_x = 0
-				trans_z = 0
-			'laid_front':
-				direction = -1
-				rotate_axis = 'z'
-				trans_y = 3.5#era 2
-				next_position = 'laid_upsidedown'
-				trans_x = -13.5#era 13
-			"laid_upsidedown":
-				direction = -1
-				rotate_axis = 'z'
-				trans_y = -3
-				next_position = 'laid_back'
-				trans_x = -13
-				trans_z = 0
-		rotating = true
+		if position == "standing":
+			x_center = transform.origin.x + 0.5
+			y_center = 0.0
+			z_center = 0.0
+			x_ini = transform.origin.x
+			x_end = x_ini + 1.5
+			angulo = 0.0
+			rotating = true
 
-	if event.is_action_pressed('ui_up'):
-		print('pressed up')
-		match current_position:
-			'standing':
-				direction = 1
-				rotate_axis = 'z'
-				trans_y = 3
-				next_position = 'laid_back'
-				trans_x = 0
-				trans_z = 0
-			'laid_right':
-				pass
-			'laid_left':
-				pass
-			'laid_back':
-				pass
-			'laid_front':
-				pass
-			"laid_upsidedown":
-				pass
-		rotating = true
-		
-	if event.is_action_pressed('ui_left'):
-		print('pressed left')
-		match current_position:
-			'standing':
-				direction = 1
-				rotate_axis = 'x'
-				trans_y = 3
-				next_position = 'laid_left'
-			'laid_right':
-				pass
-			'laid_left':
-				pass
-			'laid_back':
-				pass
-			'laid_front':
-				pass
-			"laid_upsidedown":
-				pass
-		rotating = true
-		
-	if event.is_action_pressed('ui_right'):
-		print('pressed right')
-		match current_position:
-			'standing':
-				direction = -1
-				rotate_axis = 'x'
-				trans_y = 3
-				next_position = 'laid_right'
-			'laid_right':
-				pass
-			'laid_left':
-				pass
-			'laid_back':
-				pass
-			'laid_front':
-				pass
-			"laid_upsidedown":
-				pass
-		rotating = true
-
-func _physics_process(delta):#process(delta):
+func _physics_process(delta):
 	if !rotating: return
-	angulo += 2
-	
-	if angulo <= 90:
-		match rotate_axis:
-			'z':
-				#rotate_z(deg2rad(direction * 2))
-				rotate_object_local(Vector3(0, 0, 1), deg2rad(direction * 2))
-				translate_object_local(Vector3(trans_x * corr, trans_y * corr, trans_z * corr))
-			'x':
-				rotate_object_local(Vector3(1, 0, 0), deg2rad(direction * 2))
-				translate_object_local(Vector3(trans_x * corr, trans_y * corr, trans_z * corr))
-			'y':
-				pass
+	print('rotating')
+	if abs(angulo) <= 90:
+		y_ini = y_circunferencia(x_ini-x_center+0.5)
+		Position1.origin = Vector3(x_ini, y_ini, 0)
+		
+		x_end = x_ini + 0.15
+		y_end = y_circunferencia(x_end-x_center+0.5)
+		Position2.origin = Vector3(x_end, y_end, 0)
+		
+		var v1 = Vector3(x_ini, y_ini, 0) - Vector3(x_center, y_center, z_center)
+		var v2 = Vector3(x_end, y_end, 0) - Vector3(x_center, y_center, z_center)
+		var cosphi = v1.dot(v2) / (v1.length() * v2.length())
+		angulo -= rad2deg(acos(cosphi))
+		
+		print('angulo: ', angulo, ' x_ini: ', x_ini, ' y_ini: ', y_ini, ' x_end: ', x_end, ' y_end: ', y_end)
+		transform = Position1.interpolate_with(Position2, 1)
+		x_ini += 0.15
+		rotation_degrees = Vector3(0, 0, angulo)
 	else:
 		rotating = false
-		transform.origin = transform.origin.snapped(Vector3(0.5, 0.5, 0.5))
-		angulo = 0
-		current_position = next_position
-		#print('current position: ', current_position)
-		#print('next position: ', next_position)
 
+func y_circunferencia(x):
+	return sqrt(((-1) * x * x) + x + 1.0)
